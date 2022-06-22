85467c6a592b7cd3edabfe3fcd70f2c08fee0070
aisha <aishatadebayor0@gmail.com>
2022-06-22 11:05:39 -0700

    correction

---
+++ 0x06-python-classes/1-main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+square = __import__('1-square').square
+
+my_square = square(3)
+print(type(my_square))
+print(my_square.__dict__)
+
+try:
+    print(my_square.size)
+except Exception as e:
+    print(e)
+
+try:
+    print(my_square.__size)
+except Exception as e:
+    print(e)
